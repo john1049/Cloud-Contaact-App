f42e0f79418ddc9803a180181666cd4d9976089a
LAPTOP\Ryan <john1049@algonquinlive.com>
2017-03-16 19:00:11 -0400

    Add contact function added

--- app.js
+++ app.js
@@ -1,20 +1,61 @@
 angular
 .module('app', ['firebase'])
-.const('firebaseConfig'{
-      apiKey: "AIzaSyDt6DhtN242cZRCMtgA_u8hs98P1AePQAc",
-      authDomain: "my-version-of-inclass.firebaseapp.com",
-      databaseURL: "https://my-version-of-inclass.firebaseio.com",
-      storageBucket: "my-version-of-inclass.appspot.com",
-      messagingSenderId: "526132328277"
+.constant('firebaseConfig', {
+  apiKey: "AIzaSyCjBsE-TacGXrkXz-m--UmBqohOlmCbTHk",
+authDomain: "small-firebase-app.firebaseapp.com",
+databaseURL: "https://small-firebase-app.firebaseio.com",
+storageBucket: "small-firebase-app.appspot.com",
+messagingSenderId: "251817607638"
 })
 .run(firebaseConfig => firebase.initializeApp(firebaseConfig))
 .service('dbRefRoot', DbRefRoot)
 .service('contacts', Contacts)
-
+.controller('ContactCtrl', ContactCtrl)
 function DbRefRoot() {
   return firebase.database().ref()
 }
 
-function Contacts(deRefRoot, $firebaseObject, $firebaseArray){
-  
+function Contacts(dbRefRoot, $firebaseObject, $firebaseArray){
+  const dbRefContacts = dbRefRoot.child('contacts')
+
+  this.get = function get(id){
+    return $firebaseObject(dbRefContacts.child(id))
+  }
+
+  this.getAll = function getAll(){
+    return $firebaseArray(dbRefContacts)
+  }
+}
+
+function ContactCtrl(contacts){
+
+  this.clearForm = function clearForm() {
+    return {
+      firstName: '',
+      lastName: '',
+      phone: '',
+      email: '',
+      bday: ''
+    }
+  }
+
+  this.newContact = this.clearForm()
+
+  this.contacts = contacts.getAll()
+
+  this.add = function add(newContact){
+    this.contacts
+    .$add(newContact)
+    .then(newRef => {
+      this.newContact = this.clearForm()
+    })
+  }
+
+
+
+
+
+
+
+
 }
